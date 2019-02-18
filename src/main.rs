0679f99faa73ed3a855f2e6dfeaabcc8c19dceb8
George <george.marr.cobc@gmail.com>
2019-02-18 21:08:11 +0000

    fuck

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::TcpListener;
 
 fn main() {
-   let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-   for stream in listener.incoming() {
-       let stream = stream.unwrap();
+   for _stream in listener.incoming() {
+       let stream = _stream.unwrap();
        print!("Connection made...");
    }
 }
