e542accef469a7fa9f08b7b7c9ab7ba38b0904fc
George <george.marr.cobc@gmail.com>
2019-02-18 21:09:08 +0000

    end me

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net:TcpStream;
+use std::io::prelude::*;
 
 fn main() {
    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,4 +9,8 @@ fn main() {
        let stream = _stream.unwrap();
        print!("Connection made...");
    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
 }
