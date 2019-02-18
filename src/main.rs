0faf65f1ac5af34715eb5045ef58dec37494b682
George <george.marr.cobc@gmail.com>
2019-02-18 21:14:47 +0000

    read the file contents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,7 +17,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     println!("Requested {}", String::from_utf8_lossy(&buffer[..]));
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("test.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n", contents);
+    
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }   
